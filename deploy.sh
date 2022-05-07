f0668509f6e44f6c673893c249c36e763dc2c2a4
root <linera.nightwing@gmail.com>
2022-05-07 04:51:41 -0500

    update

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ set -e
 printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 
 # Build the project.
-hugo -t hugo-theme-learn # if using a theme, replace with `hugo -t <YOURTHEME>`
+hugo -t hugo-theme-relearn # if using a theme, replace with `hugo -t <YOURTHEME>`
 
 # Go To Public folder
 cd public
